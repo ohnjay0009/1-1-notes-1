b0bc085f41a247b336a43b03d8025fe05c5b45ad
OhnJay0009 <112983242+ohnjay0009@users.noreply.github.com>
2022-09-13 20:22:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#print:
+print("Hello World!")
+#cannot use uppercase
+#Print("Hello World!") (wont work)
+print("Hello")
+print("world")
+#single quotes act the same as double quotes
+print('hi')
+print("hi")
+#using end= at the end of a print statement, the text can go to the same line
+print('hello',end=" ")
+print('world',end=" ")
+#we can also use an empty print statement to drop down a line in the console
+print('hello')
+print()
+print('world')
+#end behavior using end="/n"
+print("Hello",end="\n")
+#\ are dead characters so they wont show. to make one show we have to double them
+print("Shakespeare said\"to be or not to be\"")
+print("\"")
+print("\\")
+print("/\\ //\\\ ///\\\\\\ ////\\\\\\\\")
+# print will mesh multiple items together
+print("Hello","World")
+#we can also separate items in a print statement with sep="\t"
+print("Hello","World",sep="\t")
+
+#variables:
